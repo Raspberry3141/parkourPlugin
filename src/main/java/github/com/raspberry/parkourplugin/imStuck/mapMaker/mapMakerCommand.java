9aa5c9eaa5edf28533d129eb095f304a9ee7ec17
Raspberry <masa.namba101@gmail.com>
2025-04-13 16:33:22 -0700

    finish this someday

--- src/main/java/github/com/raspberry/parkourplugin/imStuck/mapMaker/mapMakerCommand.java
+++ src/main/java/github/com/raspberry/parkourplugin/imStuck/mapMaker/mapMakerCommand.java
@@ -11,9 +11,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Objects;
-
+import java.util.*;
+//TODO:finish this feature where checkpoints are ordered instead of just tp plate.
 public class mapMakerCommand implements CommandExecutor, SqlInterface {
     ParkourIdManager pkmgr;
     configFileManager conffileman;
@@ -27,7 +26,7 @@ public class mapMakerCommand implements CommandExecutor, SqlInterface {
         Player player = (Player) commandSender;
         if (Objects.equals(strings[0], "create") && strings.length ==1) {
             long id = pkmgr.updateIdNumber();
-            initializeParkourInYml(id,strings[1], player.getDisplayName(),LocalDateTime.now());
+            initializeParkourInYml(player.getWorld().getUID(),strings[1], player.getDisplayName(),LocalDateTime.now());
             try {
                 Parkour newpk = new Parkour(id);
             } catch (ClassNotFoundException e) {
@@ -42,33 +41,40 @@ public class mapMakerCommand implements CommandExecutor, SqlInterface {
         } else if (Objects.equals(strings[0], "addcp")) {
             addcp(player);
             player.sendMessage(ChatColor.YELLOW + "Added a checkpoint");
+        } else if (Objects.equals(strings[0], "deletecp")) {
+            deletecp(player);
         }
         return false;
+    }
+
+    private void deletecp(Player player) {
+
     }
 
     private void addcp(Player player) {
         if (player.getLocation().getBlock().getType().equals(Material.GOLD_PLATE)) {
             FileConfiguration config = conffileman.getParkourConfigFile();
-            HashMap<String, Integer> checkpointList = new HashMap<>();
-            checkpointList.put("x",player.getLocation().getBlockX());
-            checkpointList.put("y",player.getLocation().getBlockY());
-            checkpointList.put("Z",player.getLocation().getBlockZ());
-            config.createSection("checkpoint",checkpointList);
+
+            ArrayList<Location> checkpointList = new ArrayList<>();
+            checkpointList.add(player.getLocation());
+
+
             conffileman.saveParkourConfigFile(config);
 
         }
     }
 
-    private void initializeParkourInYml(long id, String displayName, String author, LocalDateTime date) {
+    private void initializeParkourInYml(UUID uuid, String displayName, String author, LocalDateTime date) {
         FileConfiguration config = conffileman.getParkourConfigFile();
         config.createSection("UUID");
         config.createSection("name");
         config.createSection("author");
         config.createSection("date");
-        config.set("UUID",id);
+        config.set("UUID",uuid);
         config.set("name",displayName);
         config.set("author", author);
         config.set("date",date);
         conffileman.saveParkourConfigFile(config);
     }
+
 }
